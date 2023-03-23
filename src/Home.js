baab05c8283a5dbe84668a9e319366929fb5694f
Vineet Jadhav <vineet290403@gmail.com>
2023-03-23 16:35:54 +0530

    initial commit

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,22 @@
-import React from "react";
+import { React, useState } from "react";
 
 const Home = () => {
+  // let name = "Anushka";
+
+  const [name, setName] = useState("Anushka");
+  const [age, Setage] = useState(25);
+  const handleClick = () => {
+    setName("Vineet");
+    Setage(30);
+  };
+
   return (
     <div>
       <h1>Home</h1>
+      <p>
+        {name} and {age}
+      </p>
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 };
